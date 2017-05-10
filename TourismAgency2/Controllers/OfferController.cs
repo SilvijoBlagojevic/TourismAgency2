a017ae61d03d4e02835e18e2e3f7c0aff07e5822
Silvijo <siki_bl@yahoo.com>
2017-05-10 12:51:49 +0200

    Added search and sort by season

--- TourismAgency2/Controllers/OfferController.cs
+++ TourismAgency2/Controllers/OfferController.cs
@@ -11,17 +11,53 @@ namespace TourismAgency2.Controllers
     {
         ApplicationDbContext db = new ApplicationDbContext();
         // GET: Offer
-        public ActionResult List()
+        public ActionResult List(string searchString, string sortSeasons)
         {
-            return View(db.Offers.AsEnumerable());
+            
+
+            if(searchString != null)
+            {
+                IEnumerable<Offer> searchOffers = db.Offers.Where(x => x.Destination.Contains(searchString)).AsEnumerable();
+                ViewBag.Seasons = "Results for : " + searchString.ToString();
+                return View("List", searchOffers);
+            }
+ 
+            if (sortSeasons == null)
+            {
+                ViewBag.Seasons = "All Offers";
+                return View(db.Offers.AsEnumerable());
+            }
+            if (sortSeasons.Contains("summer"))
+            {
+                IEnumerable<Offer> summerOffers = db.Offers.Where(x => x.Seasons == Seasons.Summer);
+                ViewBag.Seasons = "Summer Offers";
+                return View("List", summerOffers);
+            }
+            if (sortSeasons.Contains("winter"))
+            {
+                IEnumerable<Offer> winterOffers = db.Offers.Where(x => x.Seasons == Seasons.Winter);
+                ViewBag.Seasons = "Winter Offers";
+                return View("List", winterOffers);
+            }
+            if (sortSeasons.Contains("autumn"))
+            {
+                IEnumerable<Offer> autumnOffers = db.Offers.Where(x => x.Seasons == Seasons.Autumn);
+                ViewBag.Seasons = "Autumn Offers";
+                return View("List", autumnOffers);
+            }
+            if (sortSeasons.Contains("spring"))
+            {
+                IEnumerable<Offer> springOffers = db.Offers.Where(x => x.Seasons == Seasons.Spring);
+                ViewBag.Seasons = "Spring Offers";
+                return View("List", springOffers);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+
         }
-
-        //public ActionResult SummerOffers()
-        //{
-        //    IEnumerable <Offer> summerOffers = db.Offers.Where(x )
-        //    return View("List", db.Offers.de)
-        //} 
-
 
         protected override void Dispose(bool disposing)
         {
